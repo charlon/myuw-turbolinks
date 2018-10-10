3b464b41536f8424e8c088da38b211d84c195f62
Charlon Palacay <charlon@gmail.com>
2018-10-09 18:56:27 -0700

    Initial Xcode project commit.

---
+++ MyUW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyUW
+//
+//  Created by Charlon Palacay on 10/9/18.
+//  Copyright © 2018 Charlon Palacay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
